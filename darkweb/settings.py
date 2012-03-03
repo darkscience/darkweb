db7c3ba5ade777627fb3622a8c906b87c3249934
Kyle Fuller <inbox@kylefuller.co.uk>
2012-03-03 00:31:26 +0000

    Fix quote vote urls

--- darkweb/settings.py
+++ darkweb/settings.py
@@ -60,7 +60,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/tmp/static-ds'
+STATIC_ROOT = '/tmp/static.darkscience.ws'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
