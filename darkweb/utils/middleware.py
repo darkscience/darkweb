d2e4a0db9fa91397648f2fce8a481901fece323d
Kyle Fuller <inbox@kylefuller.co.uk>
2012-03-10 12:53:21 +0000

    Make robots middleware case insensitive

--- darkweb/utils/middleware.py
+++ darkweb/utils/middleware.py
@@ -2,12 +2,12 @@ import re
 
 from django.http import HttpResponse
 
-UA_BOT_REGEX = re.compile(r'bot|alexa|yandex|baidu|archive|bot|seo|crawl|spider|yahoo|msn|wget|curl')
-CONTENT_SENSITIVE_REGEX = re.compile(r'<!-- sensitive -->')
+UA_BOT_REGEX = re.compile(r'bot|alexa|yandex|baidu|archive|bot|seo|crawl|spider|yahoo|msn|wget|curl', flags=re.IGNORECASE)
+CONTENT_SENSITIVE_REGEX = re.compile(r'<!-- sensitive -->', flags=re.IGNORECASE)
 
 class RobotsMiddleware(object):
     def is_bot(self, request):
-        return bool(UA_BOT_REGEX.search(request.META['HTTP_USER_AGENT'].lower()))
+        return bool(UA_BOT_REGEX.search(request.META['HTTP_USER_AGENT']))
 
     def process_request(self, request):
         if request.path.startswith('/quotes') and self.is_bot(request):
