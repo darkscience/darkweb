2a4a726aeea9a9d3a2af757810322f7525ecfdce
Kyle Fuller <inbox@kylefuller.co.uk>
2012-03-10 12:56:45 +0000

    Include a fabfile to deploy static files

---
+++ fabfile.py
@@ -0,0 +1,18 @@
+from fabric.api import *
+from fabric.contrib import project
+
+env.local_static_root = '/tmp/static.darkscience.ws'
+env.remote_static_root = '/srv/http/'
+
+env.roledefs.update({
+    'static': ['amnesia.darkscience.ws',],
+})
+
+@roles('static')
+def deploy_static():
+    local('python darkweb/manage.py collectstatic')
+    project.rsync_project(
+        remote_dir = env.remote_static_root,
+        local_dir = env.local_static_root,
+        delete = True
+    )
