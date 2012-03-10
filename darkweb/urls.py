e104298f02579a7512e4462231a6f244589ba231
Kyle Fuller <inbox@kylefuller.co.uk>
2012-03-10 12:16:20 +0000

    Add a robots.txt to block quotes

--- darkweb/urls.py
+++ darkweb/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.http import HttpResponse
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -16,4 +17,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^robots\.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /quotes/", mimetype="text/plain")),
 )
