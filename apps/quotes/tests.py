d8328673cfc0e09c63d2aa3d008d7effa010d751
Kyle Fuller <inbox@kylefuller.co.uk>
2012-05-08 18:23:28 +0100

    Quotes: Add test cases for line parsing

--- apps/quotes/tests.py
+++ apps/quotes/tests.py
@@ -1,16 +1,29 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from quotes.models import Line
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def check_message(self, line):
+        line = Line.parse(line)
+        self.assertEqual(line.sender, 'kylef')
+        self.assertEqual(line.message, 'hi there')
+        self.assertEqual(line.is_action, False)
+
+    def check_action(self, line):
+        line = Line.parse(line)
+        self.assertEqual(line.sender, 'kylef')
+        self.assertEqual(line.message, 'loves github')
+        self.assertEqual(line.is_action, True)
+
+    def test_line_parse_irssi_message(self):
+        self.check_message('18:00 <kylef> hi there')
+
+    def test_line_parse_irssi_action(self):
+        self.check_action('18:00  * kylef loves github')
+
+    def test_line_parse_textual_message(self):
+        self.check_message('[18:06:49] <~kylef> hi there')
+
+    def test_line_parse_textual_action(self):
+        self.check_action('[18:07:08] kylef loves github')
+
