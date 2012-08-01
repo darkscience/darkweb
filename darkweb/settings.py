9fd028171b2e6d211a689a6be96ebb785cac0e43
Kyle Fuller <inbox@kylefuller.co.uk>
2012-08-01 13:48:55 +0100

    Use DummyCache when debugging

--- darkweb/settings.py
+++ darkweb/settings.py
@@ -30,6 +30,9 @@ CACHES = {
     }
 }
 
+if DEBUG:
+    CACHES['default']['BACKEND'] = 'django.core.cache.backends.dummy.DummyCache'
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
