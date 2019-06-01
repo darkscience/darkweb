7afeb3aab5d54b4638c658093774fd5b28bc8b70
Jan Harasym <jan.harasym@massive.se>
2019-06-01 13:28:40 +0200

    fix: templates array needed in later versions of DJANGO

--- darkweb/settings.py
+++ darkweb/settings.py
@@ -22,6 +22,22 @@ CACHES = {
     }
 }
 
+TEMPLATES = [
+  {
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [],
+    'APP_DIRS': True,
+    'OPTIONS': {
+      'context_processors': [
+        'django.template.context_processors.debug',
+        'django.template.context_processors.request',
+        'django.contrib.auth.context_processors.auth',
+        'django.contrib.messages.context_processors.messages',
+      ],
+    },
+  },
+]
+
 if DEBUG:
     CACHES['default']['BACKEND'] = 'django.core.cache.backends.dummy.DummyCache'
 
